7b093fd4a4f2732c3231bdae25228756ffe78793
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-09-23 22:08:08 -0300

    vou tentar mais

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿decimal gastoNoMes, investimento;
+double dividendYield;
+
+Console.WriteLine("-Adulto Simulator-\n");
+
+Console.Write("Gastos mensais (em R$)...:");
+gastoNoMes = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Rendimentos mensais (%)...: ");
+dividendYield = Convert.ToDouble(Console.ReadLine());
+
+investimento = gastoNoMes / Convert.ToDecimal(dividendYield / 100);
+
+Console.WriteLine($"\nPara receber {gastoNoMes:C} por mês com rendimentos de {dividendYield:N1}% você precisa ter investido {investimento:C}.");
